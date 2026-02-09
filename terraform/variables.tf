52f97307f53f0d5fea9104c70cb13e4660fe30d3
mac <samuelitiola.uk@gmail.com>
2026-02-09 22:39:26 +0000

    Add instance_name default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,14 +5,17 @@ variable "region" {
 
 variable "ami" {
   type = string
+  default = "ami-018ff7ece22bf96db"
 }
 
 variable "instance_type" {
   type = string
+  default = "t3.micro"
 }
 
 variable "instance_name" {
-  type = string
+  type    = string
+  default = "ec2-server"
 }
 
 variable "public_key" {
